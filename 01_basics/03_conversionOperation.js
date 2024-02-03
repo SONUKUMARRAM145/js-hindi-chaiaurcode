31a646601f08ae8480a53b1038b1dbad946cdb34
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2024-02-03 10:52:18 +0000

    some more about conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,34 +1,69 @@
-let score=33
+// let score=33
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
-// if anything written inside "" considered as string
-let age=true
-console.log(typeof age)
-// coverting string into number
-let valueInNumber=Number(age)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// // if anything written inside "" considered as string
+// let age=true
+// console.log(typeof age)
+// // coverting string into number
+// let valueInNumber=Number(age)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
-//"33"=>33
-//"33abc"=>NaN
-//true=>1
-//undefined=>Nan
+// //"33"=>33
+// //"33abc"=>NaN
+// //true=>1
+// //undefined=>Nan
 
-let isLoggedIn="sonu"
+// let isLoggedIn="sonu"
 
-let booleanisloggedin=Boolean(isLoggedIn)
-console.log(typeof booleanisloggedin)
-console.log(booleanisloggedin)
+// let booleanisloggedin=Boolean(isLoggedIn)
+// console.log(typeof booleanisloggedin)
+// console.log(booleanisloggedin)
 // 1=>true; 0=>false
 //""=>false
 //"sonu"=>true
  
 
-let rollno=33;
-let stringroll=String(rollno);
-console.log(typeof stringroll);
-console.log(stringroll);
+// let rollno=33;
+// let stringroll=String(rollno);
+// console.log(typeof stringroll);
+// console.log(stringroll);
 
+// ****************** Opeartions **************
+
+// let value=3
+// let negValue= -value
+// console.log(negValue)
+
+// console.log(3+3);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // 2 to the power 3
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1="sonu";
+// let str2="kumar";
+// let str3=str1+" "+str2
+// //console.log(str3)
+
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(true);
+// console.log(+true);// don't use this type of code
+// console.log(true+);// not accepted
+
+let gamecounter=100
+
+gamecounter++;
+console.log(gamecounter)
+
+let num1=4
+++num1;
+console.log(num1) 
+
+// learn about prefix and postfix from mdn go and read the documentation
 

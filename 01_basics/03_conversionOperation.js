802f4dea90c0dbe1b53fef73211ee7e49f823127
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2024-02-03 10:31:14 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score=33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+// if anything written inside "" considered as string
+let age=true
+console.log(typeof age)
+// coverting string into number
+let valueInNumber=Number(age)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//undefined=>Nan
+
+let isLoggedIn="sonu"
+
+let booleanisloggedin=Boolean(isLoggedIn)
+console.log(typeof booleanisloggedin)
+console.log(booleanisloggedin)
+// 1=>true; 0=>false
+//""=>false
+//"sonu"=>true
+ 
+
+let rollno=33;
+let stringroll=String(rollno);
+console.log(typeof stringroll);
+console.log(stringroll);
+
+

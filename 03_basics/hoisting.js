7a6dea327c1f017303eabfa5cc76f345b586dd47
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2024-03-04 14:39:18 +0000

    hoisting

--- 03_basics/hoisting.js
+++ 03_basics/hoisting.js
@@ -1,1 +1,12 @@
 // scope level and mini hoisting
+addone(5);
+function addone(value) {
+    console.log(value);
+}
+// but this will not work give u error
+
+console.log(value);
+const value=function (num) {
+    return num+1;
+}
+

45e62228c63cfd779969bcde465285ac5a25411e
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2024-03-07 14:52:49 +0000

    controlflow

---
+++ 04_controlflow/one.js
@@ -0,0 +1,9 @@
+// control flow also called logic flow
+if(2==="2"){
+ // if true then code execute
+ console.log(`hello`); 
+}
+else{
+    console.log(`lelo`);
+}
+// <,>,<=,>=,==,!=,=== this will check data type also

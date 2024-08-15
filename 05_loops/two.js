36f1ad12a8bcc036e615b3d0c1f11ddaa54c15b8
SONU KUMAR RAM <92182339+SONUKUMARRAM145@users.noreply.github.com>
2024-08-15 06:44:52 +0000

    while, do while

---
+++ 05_loops/two.js
@@ -0,0 +1,19 @@
+let index=0;
+while(index<=10){
+    console.log(`value of index is ${index}`);
+    index++;
+}
+
+let myArray=['flash','batman','superman']
+let arr=0;
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`);
+    arr++;
+}
+
+// do while ,ek baar toh chlega hi before checking the condition
+let score=1;
+do{
+    console.log(`score is${score}`);
+    score++;
+}while (score<=10);
